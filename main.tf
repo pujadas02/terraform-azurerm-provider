c65ffc3a73d1a071584cb89313f2a52915f9ed5b
pujadas02 <pd4759150@gmail.com>
2025-02-06 16:02:21 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,18 @@
+# terraform-azurerm-provider/main.tf
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.15.0"  # Pinning the version to avoid accidental upgrades
+    }
+  }
+  required_version = ">= 1.3.0"
+}
+
+provider "azurerm" {
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = true
+    }
+  }
+}
